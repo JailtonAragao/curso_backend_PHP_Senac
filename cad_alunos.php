c4a1cfa7a40cbf79cd1447467714abd5db3151a6
jailton aragao <jailton.aragao@gmail.com>
2022-09-27 15:21:14 -0300

    conectado banco ao frontend

--- cad_alunos.php
+++ cad_alunos.php
@@ -1,3 +1,39 @@
+<?php
+// if de esperar aguardando o submit para rodar o codigo
+if(isset($_POST['submit'])){
+  include_once 'conexao.php';
+  // transformando uma variavél superglobal em local
+  $nome = $_POST['nome'];
+  $escolaridade = $_POST['escolaridade'];
+  $serie = $_POST['serie'];
+  $email = $_POST['email'];
+  $data_nasc = $_POST['data_nasc'];
+  $telefone = $_POST['telefone'];
+  $matricula = $_POST['matricula'];
+
+  $sql = "INSERT INTO alunos(
+    nome,
+    email,
+    serie,
+    matricula,
+    data_nasc,
+    escolaridade,
+    telefone)
+    VALUES('$nome','$email','$serie','$matricula','$data_nasc','$escolaridade',' $telefone')";
+
+
+  $resultado = $conexao->query($sql);  
+
+}
+
+?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -71,39 +107,39 @@
   <main>
   <section class="box">
 
-  <form action="cad_alunos" method="$_POST">
+  <form action="cad_alunos.php" method="POST">
   <div class="row mb-3">
-    <label for="inputEmail3" class="col-sm-2 col-form-label" name="nome">Nome</label>
+    <label for="inputEmail3" class="col-sm-2 col-form-label" >Nome</label>
     <div class="col-sm-10">
-      <input type="text" class="form-control" id="inputEmail3">
+      <input type="text" class="form-control" id="inputEmail3" name="nome">
     </div>
   </div>
 
   <div class="row mb-3">
-    <label for="inputEmail3" class="col-sm-2 col-form-label" name="email">Email</label>
+    <label for="inputEmail3" class="col-sm-2 col-form-label" >Email</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3">
+      <input type="email" class="form-control" id="inputEmail3" name="email">
     </div>
   </div>
 
   <div class="row mb-3">
-    <label for="inputEmail3" class="col-sm-2 col-form-label" name="serie">Serie</label>
+    <label for="inputEmail3" class="col-sm-2 col-form-label" >Serie</label>
     <div class="col-sm-10">
-      <input type="number" class="form-control" id="inputEmail3">
+      <input type="number" class="form-control" id="inputEmail3" name="serie">
     </div>
   </div>
 
   <div class="row mb-3">
-    <label for="inputEmail3" class=" col-form-label" name="matricula">Matricula</label>
+    <label for="inputEmail3" class=" col-form-label" >Matricula</label>
     <div class="col-sm-10">
-      <input type="number" class="form-control" id="inputEmail3">
+      <input type="number" class="form-control" id="inputEmail3" name="matricula">
     </div>
   </div>
 
   <div class="row mb-3">
-    <label for="inputEmail3" class=" col-form-label" name="data_nasc">Data de Nascimento</label>
+    <label for="inputEmail3" class=" col-form-label" >Data de Nascimento</label>
     <div class="col-sm-10">
-      <input type="date" class="form-control" id="inputEmail3">
+      <input type="date" class="form-control" id="inputEmail3" name="data_nasc">
     </div>
   </div>
 
@@ -127,14 +163,19 @@
 
     
   <div class="row mb-4 me-9">
-    <label for="inputEmail3" class="col-sm-2 col-form-label" name="telefone">Telefone</label>
+    <label for="inputEmail3" class="col-sm-2 col-form-label" >Telefone</label>
     <div class="col-sm-11">
-      <input type="tel" class="form-control" id="inputEmail3">
+      <input type="tel" class="form-control" id="inputEmail3" name="telefone">
     </div>
   </div>
   </fieldset>
   
-  <button type="submit" class="btn btn-warning" name="submit">CADASTRAR</button>
+  <input type="submit" class="btn btn-warning" name="submit"></input>
+  <?php
+  if (!empty($resultado)){
+    echo "Enviado ok";
+  }
+?>
 </form>
   </section>
   </main>
